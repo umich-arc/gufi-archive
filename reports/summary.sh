a78bb997f5065d95a3826880f21f61d7be4e2f7f
Brock Palen <brockp@umich.edu>
2022-06-02 15:13:25 -0400

    summary fixes

--- reports/summary.sh
+++ reports/summary.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 THREADS=4
 BFQ="singularity exec gufi_master.sif gufi_query"
-QUERYDBS="singularity exec gufi_master.sif querydbs"
+QUERYDBS="singularity exec --bind /etc/passwd gufi_master.sif querydbs"
+DAYS=${2:-180}
 
-$BFQ -E "insert into sument select uidtouser(uid, 0), name, size, mtime from entries where type='f'and size>4;" -n $THREADS -O outdb -I "create table sument (username text, name text, size int64, mtime int64);" $1
+echo ""
+echo "Using GUFI Index located in: $1"
+echo "Reporting on data older than $DAYS days last accessed"
+echo ""
 
-$QUERYDBS -NV outdb sument "select username, name,size/1024/1024,datetime(mtime, 'unixepoch') from vsument order by size;" outdb.*
+$BFQ -E "insert into sument select uid, name, size, atime from entries where type='f'and size>4 AND datetime(atime, 'unixepoch') < DATE('now', '-"$DAYS" day');" -n $THREADS -O outdb -I "create table sument (username text, name text, size int64, atime int64);" $1
+
+$QUERYDBS -NV outdb sument "select uidtouser(username, 0) AS username, COUNT(*) AS count, sum(size)/1024/1024/1024 AS sizeGB from vsument GROUP BY uidtouser(username, 0) ORDER BY sizeGB DESC;" outdb.* | column -s '|' -t
+#$QUERYDBS -NV outdb sument "select name,username, size from vsument WHERE username NOT LIKE 'brockp';" outdb.*
 
 # cleanup 
 rm -f outdb.*
